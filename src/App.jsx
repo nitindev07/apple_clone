5c8481535a4d1e5ba1ba8a5f5e0ab6dd37de9dc8
Nitindev07 <nitin.dev2907@gmail>
2024-08-08 21:46:36 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Model from "./components/Model"
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
+
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
+    </main>
+  )
+}
+
+
+export default App;
